1e4ea063982fde89fcb00a70c951c49a1c07ff39
reuvenp <reuvenp@checkpoint.com>
2016-11-05 22:44:10 +0200

    branch schema

--- server.js
+++ server.js
@@ -12,22 +12,11 @@ var db = mongo.connect('mongodb://localhost:27017/myseconddatabase');
 //db.once('open', function() { console.log("Connected to DB") });
 //db.on('error', function() {  console.log("Error connecting to DB") });
 console.log('Pending DB connection');
-/*var branch = new Branch({
-    name: 'Main Branch',
-    location: 'Herzl 74 Tel Aviv',
-    isActive: true,
-    openingHours: 'Sun - Thu, 10:00 - 17:00'
-});
-branch.save(function (err) {
-    if (err) throw err;
-    console.log('suc');
-});*/
 Branch.find({}, function(err, branches) {
     if (err) throw err;
     // object of all the branches
     console.log(branches);
 });
-//AddBranch('Second Branch', 'Hayarkon 24 Bnei Brak', 'Sun - Thu, 10:00 - 18:30');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.get('/', function (req, res) {
